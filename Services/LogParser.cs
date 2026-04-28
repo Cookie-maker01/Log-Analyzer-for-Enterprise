d260d7da4b527f350cbcf7b87b0a428d825c3c82
unknown <weifang060@gmail.com>
2026-04-28 15:44:32 +1200

    Modify Logparser file

--- Services/LogParser.cs
+++ Services/LogParser.cs
@@ -12,16 +12,23 @@ namespace LogAnalyzer.Services
 
             foreach(var line in lines)
             {
-                var parts = line.Split(' ', 2); // depart Level + Message
+                var parts = line.Split(' ', 4);
 
-                if(parts.Length == 2)
+                if (parts.Length < 4) continue;
+
+                var timestamp = DateTime.Parse($"{parts[0]} {parts[1]}");
+                var level = parts[2];
+                var message = parts[3];
+
+                logs.Add(new LogEntry
                 {
-                    logs.Add(new LogEntry
-                    {
-                        Level = parts[0],
-                        Message = parts[1]
-                    });
-                }
+                    Timestamp = timestamp,
+                    Level = level,
+                    Message = message
+                });
+            }
+
+            return logs;
             }
 
             return logs;
