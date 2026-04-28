ec16ce6338c8b04f75d474a8c26135c0479160d4
unknown <weifang060@gmail.com>
2026-04-28 16:13:26 +1200

    Modify Program file

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 ﻿using LogAnalyzer.Services;
 
-var filePath = "log.txt";
+var filePath = Path.Combine(AppContext.BaseDirectory, "log.txt");
+
+//1. parse
 
 var parser = new LogParser();
 
 var logs = parser.Parse(filePath);
 
+//2. filter
+
 var analyzer = new LogAnalyzerService();
+logs = analyzer.FilterByTime(logs, DateTime.Nwo.AddHours(-1));
+
+//3. analyze
 
 analyzer.Analyze(logs);
+
+//4. generate JSON report
+var reportService = new LogReportService();
+reportService.GenerateReport(logs);
 
 Console.ReadLine();
